a3c04ea43250c32a3aa099f022cfc6cdf8c925bf
Randy Tolentino <makertolentino@gmail.com>
2016-11-29 03:37:47 -0600

    :rocket: first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Standups Bot (for Slack)
---
+++ app.js
@@ -0,0 +1,17 @@
+
+var express = require('express');
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  console.log("server starting on " + appEnv.url);
+});
